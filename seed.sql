2b1a1b52a9311dcc3f0a1b3c95d4757cac1dee64
Christian Killelea <christiankillelea@Christians-MacBook-Air.local>
2023-06-12 00:12:09 -0500

    commit

--- seed.sql
+++ seed.sql
@@ -1,10 +1,8 @@
-/* Department table */
 INSERT INTO department (name) VALUES
   ('Sales'),
   ('Accounting'),
   ('Engineering');
 
-/* Employee table */
 INSERT INTO employee (first_name, last_name, job_id, manager_id) VALUES
   ('Thierry', 'Henry', 1, NULL),
   ('Gabriel', 'Jesus', 2, 1),
